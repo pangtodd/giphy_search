ed8cd8a174f6c1e33fecdb254aa5dda2e6bcf1d7
Todd Pangilinan <pang.todd@gmail.com>
2022-03-16 22:46:47 -0700

    update index.js, modified the code from the weather api project to work with Giphy.

--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,33 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './../css/styles.css';
 import $ from 'jquery';
+import GitService from './gifSearch.js';
+
+function clearFields() {
+  $('#search').val("");
+  $('.showErrors').text("");
+  $('.showGif').text("");
+  $('.showDescription').text("");
+  // $('.showGif').text("");
+}
+
+$(document).ready(function() {
+  $('#results').click(function() {
+    const search = $('#search').val();
+    // $('#city').val("");
+    // $('#state').val("");
+    clearFields();
+    let promise = GitService.getGif(search);
+    promise.then(function(response) {
+      const body = JSON.parse(response);
+      $('.showGif').append(`<img src = "${body.data[0].images.downsized.url}">`);
+      $('.showDescription').text(`${body.data[0].title}`);
+    }, function(error) {
+      $('.showErrors').text(`There was an error processing your request: ${error}`);
+    });
+  });
+});
+
+/*
+<iframe src=`${body.data[0].images.downsized.url}` width="480" height="357" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href=`${body.data[1].images.downsized.url}`>via GIPHY</a></p>
+*/
